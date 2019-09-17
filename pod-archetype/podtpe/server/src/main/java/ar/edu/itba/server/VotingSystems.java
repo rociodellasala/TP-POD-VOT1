19a786a3a8b918e3daf95e563f3dadd996a95dcf
asantoflaminio <asantoflaminio@itba.edu.ar>
2019-09-17 13:54:22 -0300

    break fix

--- pod-archetype/podtpe/server/src/main/java/ar/edu/itba/server/VotingSystems.java
+++ pod-archetype/podtpe/server/src/main/java/ar/edu/itba/server/VotingSystems.java
@@ -298,6 +298,7 @@ public class VotingSystems {
 						winner = p;
 						totalWinners++;	
 						currentlyInRace.remove(p); // lo saco de la carrera, pues ya gano
+						break;
 					}
 					
 					if (leastVoted == null) {
